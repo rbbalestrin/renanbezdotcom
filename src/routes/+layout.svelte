8ea60c03321aa3b5abf927ad84e743389b5f2887
rbbalestrin <therealrenanbez@gmail.com>
2024-12-17 17:05:43 -0300

    meta changed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,9 +25,9 @@
     <meta property="og:image" content="https://www.renanbez.com/og.png" />
 
     <meta name="twitter:card" content="summary_large_image" />
-    <meta property="twitter:domain" content="luke.boo" />
+    <meta property="twitter:domain" content="renanbez.com" />
     <meta property="twitter:url" content="https://www.renanbez.com/" />
-    <meta name="twitter:title" content="luke" />
+    <meta name="twitter:title" content="renan" />
     <meta name="twitter:description" content="fullstack designer" />
     <meta name="twitter:image" content="https://www.renanbez.com/og.png" />
 </svelte:head>
